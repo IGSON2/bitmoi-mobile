61f67081a15777a60709b1d6fb25f91e77517c03
IGSON2 <suninnaa@gmail.com>
2024-01-05 00:24:12 +0900

    update oauth url fetcher

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,14 +1,21 @@
+import { useEffect, useState } from "react";
 import Pagination from "../components/Pagination";
 import { PageId, PageInfo } from "../types/types";
 import "./Mypage.css";
+import { axiosClient } from "../utils/axiosClient";
 
 export const Mypage = () => {
+    const [loginURL, setLoginURL] = useState<string>("");
+    useEffect(() => {
+        axiosClient.get("/oauth").then((res) => setLoginURL(res.data));
+    },[]);
     const pageInfo:PageInfo = {
         pageId: PageId.MyPage,
     }
     return (
         <div className="mypage">
             <h1>Mypage</h1>
+            <a href={loginURL}>로그인</a>
             <Pagination {...pageInfo} /> {/* 객체의 전개 연산자 */}
         </div>
     );

986930369d03db16d554d374c90b3877ee7f45c5
IanJo-Rigo <91243660+IGSON2@users.noreply.github.com>
2024-01-24 17:02:49 +0900

    update server file

---
+++ server/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/rs/zerolog"
+	zlog "github.com/rs/zerolog/log"
+)
+
+const (
+	// port = ":443"
+	port = ":80"
+)
+
+var (
+	allowOriginMiddleware = cors.New(cors.Config{
+		AllowOrigins: "*",
+	})
+	limiterMiddleware = limiter.New(limiter.Config{
+		Max:        30,
+		Expiration: 30 * time.Second,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.Get("x-forwarded-for")
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusTooManyRequests).SendString("too many request.")
+		},
+	})
+	loggerMiddleware = logger.New(logger.Config{
+		Format:     "[${ip}]:${port} ${time} ${status} - ${method} ${path} - ${latency}\n",
+		TimeFormat: "2006-01-02T15:04:05",
+	})
+
+	redirectMiddleware = func(c *fiber.Ctx) error {
+		zlog.Info().Msgf(`[%s]:%s Redirected from "%s://%s%s" to https`, c.IP(), c.Port(), c.Protocol(), c.Hostname(), c.OriginalURL())
+		return c.Redirect("https://"+c.Hostname()+c.OriginalURL(), fiber.StatusMovedPermanently)
+	}
+)
+
+func init() {
+	zlog.Logger = zlog.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+}
+
+func main() {
+	// noTLSApp := fiber.New()
+	// noTLSApp.Use(redirectMiddleware, loggerMiddleware)
+	// go func() {
+	// 	log.Fatalln(noTLSApp.Listen(":80"))
+	// }()
+	app := fiber.New()
+
+	app.Use(allowOriginMiddleware, limiterMiddleware, loggerMiddleware)
+
+	app.Static("/", "./build")
+	app.Static("/invest/*", "./build")
+	app.Static("/mypage", "./build")
+	app.Static("/rank", "./build")
+	app.Static("/login", "./build")
+	app.Static("/welcome", "./build")
+	// log.Fatalln(app.ListenTLS(port, "./server.crt", "./server.key"))
+	log.Fatalln(app.Listen(port))
+
+}

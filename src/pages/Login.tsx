66e3b4e37c0a88ece23191c48b562e80f4b1568a
Igson <suninnaa@gmail.com>
2024-03-01 22:50:11 +0900

    update kakao oauth

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,33 @@
 import { useLocation, useParams } from "react-router-dom";
 import "./Login.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Login = () => {
   const { req_url } = useParams();
   const loc = useLocation();
+
+  const [isKakao, setIsKakao] = useState<boolean>(false);
 
   useEffect(() => {
     const recommender = new URLSearchParams(loc.search).get("recommender")!;
     if (recommender) {
       localStorage.setItem("recommender", recommender);
     }
+    navigator.userAgent.includes("KAKAOTALK")
+      ? setIsKakao(true)
+      : setIsKakao(false);
   }, []);
 
-  function login() {
+  function googleLogin() {
     const apiURL = process.env.REACT_APP_API_URL || "https://api.bitmoi.co.kr";
-    window.location.href = `${apiURL}/oauth/${req_url}`;
+    window.location.href = `${apiURL}/oauth/${req_url}?platform=google`;
   }
+
+  function kakaoLogin() {
+    const apiURL = process.env.REACT_APP_API_URL || "https://api.bitmoi.co.kr";
+    window.location.href = `${apiURL}/oauth/${req_url}?platform=kakao`;
+  }
+
   return (
     <div className="login">
       <img
@@ -30,14 +41,24 @@ export const Login = () => {
       <h3 className="login_title">Bitmoi</h3>
       <img className="login_logo" alt="logo" src="/images/bitmoi.png" />
       <div className="login_info">
-        {"비트모이는 구글 이메일이 있어야 서비스 이용이 가능합니다."}
+        {"비트모이는 간편 로그인을 통한 가입을 지원합니다."}
       </div>
-      <img
-        className="login_oauth"
-        src="/images/google_login.png"
-        alt="google_login"
-        onClick={login}
-      />
+      <div className="login_oauth_wrapper">
+        {!isKakao ? (
+          <img
+            className="login_oauth"
+            src="/images/google_login.png"
+            alt="google_login"
+            onClick={googleLogin}
+          />
+        ) : null}
+        <img
+          className="login_oauth"
+          src="/images/kakao_login.png"
+          alt="kakao_login"
+          onClick={kakaoLogin}
+        />
+      </div>
     </div>
   );
 };
